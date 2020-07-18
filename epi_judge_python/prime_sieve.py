91744c3e15887bed76f85957ff284fb38c45fa97
Ananya Paul <ap2273@cornell.edu>
2020-07-17 18:03:50 -0700

    prime_sieve.py

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -5,8 +5,19 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    isPrime = [True]*(n+1)
+    isPrime[0] = False
+    isPrime[1] = False
+    primes = []
+
+    for i in range(2, n+1):
+        if isPrime[i] == True:
+            primes.append(i)
+            # seive
+            for j in range(i+i, n+1, i):
+                isPrime[j] = False
+
+    return primes
 
 
 if __name__ == '__main__':
